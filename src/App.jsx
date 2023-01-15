f690da49db31ccbbcefef4b2f8c828a78e1a4423
JBatSWK <j.bechler@stadtwerke-konstanz.de>
2023-01-15 18:12:39 +0100

    added files

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import config from "../../config.json"
+
+import './App.css'
+import './SWK_React_Template/src/styles/App.css'
+
+import Main from './SWK_React_Template/src/components/Main/Main';
+
+function App() {
+	const name = "Konrad";
+	const icon = require("./Bicycle.png");
+	config.port = config.konrad.port
+	config.port_react = config.konrad.port_react
+
+	return (
+
+		<Main name={name} icon={icon} config={config}></Main>
+
+	);
+}
+
+export default App;
